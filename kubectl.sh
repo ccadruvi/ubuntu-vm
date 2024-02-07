3a1eb2694e17177ef94468cd06f1932f70add46c
Christian Cadruvi <Christian.Cadruvi@digitecgalaxus.ch>
2024-02-07 09:29:25 +0100

    Update terraform install script

--- kubectl.sh
+++ kubectl.sh
@@ -4,7 +4,7 @@ set -uo pipefail
 if ! command -v kubectl &> /dev/null ; then
   mkdir -p /etc/apt/keyrings
   sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-  echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list  
+  echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
  
   sudo apt-get update
   sudo apt-get install -y kubectl
